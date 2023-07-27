fb5faeee9bf69b9cf0cec7a1bd4d5fcd0d89ea7f
Niko Lazaris <nlazaris@drift.com>
2023-07-27 15:27:31 -0400

    add docs page

---
+++ components/Navigation.js
@@ -0,0 +1,30 @@
+import Link from "next/link"
+import Image from "next/image"
+
+const Navigation = () => {
+  return (
+    <div className="w-[48px] fixed drop-shadow-sm bg-white border-r h-screen flex flex-col items-center">
+      <div className="flex justify-center items-center w-[48px] h-[86px] py-6 bg-ai-purple">
+        <Image src="/drift.svg" width={24} height={24} />
+      </div>
+      <div className="flex flex-col items-center flex-1">
+        <Link href="/chat">
+          <div className="px-3 py-5 hover:bg-slate-100 cursor-pointer">
+            <Image src="/sparkles.svg" width={24} height={24} />
+          </div>
+        </Link>
+        <Link href="/docs">
+          <div className="px-3 py-5 hover:bg-slate-100 cursor-pointer">
+            <Image src="/document.svg" width={24} height={24} />
+          </div>
+        </Link>
+      </div>
+
+      <div className="flex flex-col items-center my-5 w-[28px] h-[28px] border border-2 border-white rounded-full">
+        <Image src="/user.svg" width={28} height={28} />
+      </div>
+    </div>
+  )
+}
+
+export default Navigation
---
+++ pages/chat.js
@@ -0,0 +1,10 @@
+import Navigation from "../components/Navigation"
+
+export default function ChatPage({ documentSets }) {
+  return (
+    <div className="flex">
+      <Navigation />
+      <div className="px-20">Chat Page</div>
+    </div>
+  )
+}
